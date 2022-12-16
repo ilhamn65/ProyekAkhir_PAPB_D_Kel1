425f5c97bcd5529633a841eaf3648b45339cbbed
Ilham Nuansa <77243888+ilhamn65@users.noreply.github.com>
2022-12-16 16:36:01 +0700

    first commit

---
+++ app/src/main/java/com/example/proyek_akhir_papb/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.proyek_akhir_papb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loading);
+    }
+}
